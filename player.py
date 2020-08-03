65d5a57027e1e31eb8766832bfb4b45b1fd888ff
Wasim Ashraf <wasim@anazeem.com>
2020-08-03 17:17:34 +0300

    Player-Game score

--- player.py
+++ player.py
@@ -1,15 +1,22 @@
 player_1 = 'Sam'
 player_2 = 'Alex'
 current_round = 1
-current_round = 2
+#current_round = 2
 print("Game on!")
-print(f"player 1: {player_1}")
-print(f"player 2: {player_2}")
+print(f"player_1: {player_1}")
+print(f"player_2: {player_2}")
 print("-------------------")
-print(f"Round:{current_round}")
-player_1_score = 15
+
+print(f"Round:{current_round}!")
+player_1_score = 10
 player_2_score = 13
-print(f"player 1 score:{player_1_score}" )
-print(f"player 2 score:{player_2_score}")
-print(f"{player_1} wins with {player_1_score} to {player_2_score}")
+print(f"player-2 score:{player_2_score}" )
+print(f"player_1 score:{player_1_score}")
+print(f"{player_2} wins with {player_2_score} to {player_1_score}")
 print("-------------------")
+
+print(f"Round:{current_round + 1}!")
+player_1_score = 20
+player_2_score = 5
+print(f"{player_1} defeat {player_2} by {player_1_score-player_2_score}")
+
